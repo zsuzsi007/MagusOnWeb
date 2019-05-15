a5d05371cb062f2fd2d7c4984359b4a4728baa53
zsuzsi007 <48222336+zsuzsi007@users.noreply.github.com>
2019-05-15 18:42:28 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+"# MagusOnWeb" 
+"# MagusOnWeb" 
+"# MagusOnWeb" 
---
+++ scripts.js
@@ -0,0 +1,12 @@
+//Disable radio buttns
+//$("input[class=gomb]").attr('disabled', true);
+
+//const gombok = document.getElementsByClassName("gomb").disabled;
+//const gomb = document.getElementById("radio2").disabled;
+
+//const newHeading = document.getElementsByTagName("h1");
+//newHeading.textContent = "Új";
+//newHeading.innerHTML = "<h1>Test</h1>";
+
+
+//window.alert('Hallo Welt!');
